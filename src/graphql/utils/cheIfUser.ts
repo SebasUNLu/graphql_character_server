b01b10d547b58380ba80f4984dd5d32ca97ff2f9
SebasUNLu <SebasUNLu@gmail.com>
2023-09-08 15:20:18 -0300

    Added graphql interface Error

--- src/graphql/utils/cheIfUser.ts
+++ src/graphql/utils/cheIfUser.ts
@@ -4,7 +4,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const checkIfUser = async (userId: number | null) => {
-  console.log(userId);
   if (!userId)
     throw new AuthenticationError('Invalid Token')
   const foundUser = await prisma.user.findUnique({
@@ -12,7 +11,6 @@ export const checkIfUser = async (userId: number | null) => {
       id: userId
     }
   })
-  console.log(foundUser);
   if (!foundUser)
     throw new AuthenticationError('Invalid Token')
   return userId
